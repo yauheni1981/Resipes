5696da059ea377587da7a37bd449176e31ee8615
yauheni1981 <93789153+yauheni1981@users.noreply.github.com>
2022-09-18 11:26:03 +0300

    Create IdHolder.java

---
+++ IdHolder.java
@@ -0,0 +1,12 @@
+@@ -0,0 +1,11 @@
+package recipes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class IdHolder {
+
+    private Integer id;
+}
